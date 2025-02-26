6ff7a708ff00beab5e75d46fe22f06cbb7127c0c
VillageR88 <villager88pl@gmail.com>
2025-02-27 00:50:25 +0100

    Layout improvement
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  helper_method :current_page
+
+  private
+
+  def current_page
+    controller_name
+  end
 end
