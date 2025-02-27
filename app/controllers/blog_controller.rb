c8c04cdb47c4b832990025e4f69e15f10a002c90
VillageR88 <villager88pl@gmail.com>
2025-02-27 09:22:46 +0100

    Initial nested blog articles
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,5 +1,19 @@
 class BlogController < ApplicationController
   def index
-    @articles = JSON.parse(File.read(Rails.root.join('db', 'data.json')))
+    @articles = load_articles
+  end
+
+  def show
+    @article = load_articles.find { |article| article['slug'] == params[:slug] }
+    
+    if @article.nil?
+      render file: "#{Rails.root}/public/404.html", status: :not_found
+    end
+  end
+
+  private
+
+  def load_articles
+    JSON.parse(File.read(Rails.root.join('db', 'data.json')))
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,13 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
+  # Blog routes
+  get 'blog', to: 'blog#index'
+  get 'blog/:slug', to: 'blog#show', as: 'blog_show'
+
   # Defines the root path route ("/")
   root "home#index"
   
-  get '/blog', to: 'blog#index'
   get '/about', to: 'about#index'
   get '/newsletter', to: 'newsletter#index'
 end
